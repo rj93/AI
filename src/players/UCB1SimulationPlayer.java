651c8f71c4e14814054342df2ed818fd8f3671d0
Richard Jones <contact@richardjones.io>
2016-11-01 12:31:30 +0000

    refactor ucb1

--- src/players/UCB1SimulationPlayer.java
+++ src/players/UCB1SimulationPlayer.java
@@ -22,60 +22,63 @@ public class UCB1SimulationPlayer extends HeuristicSimulationPlayerRefactored {
 
 	@Override
 	public void doMove() {
-		Map<GameState2P, List<Integer>> playedMoves = new HashMap<GameState2P, List<Integer>>();
+		Map<GameState2P, List<Integer>> playedStates = new HashMap<GameState2P, List<Integer>>();
 		long startTime = System.currentTimeMillis();
         long stopTime = startTime + availableTime;
         
-        List<GameState2P> legalStates = new ArrayList<GameState2P>();
+//        List<GameState2P> legalStates = new ArrayList<GameState2P>();
         int totalSims = 0;
         while (System.currentTimeMillis() < stopTime){
         	totalSims++;
         	List<Move> legalMoves = GameState2P.getLegalMoves(state, index);
         	
-        	boolean unplayedMove = false;
-        	GameState2P stateToPlay = null;
-        	for (Move m : legalMoves){
-        		GameState2P s = m.doMove(state);
-        		legalStates.add(s);
-        		if (!playedMoves.containsKey(s)){
-        			// play this move
-        			unplayedMove = true;
-        			stateToPlay = s;
-        		}
-        	}
-        	if (!unplayedMove){
-        		double ubc1 = -1;
-        		int n = 0, w = 0;
-        		List<GameState2P> l = new ArrayList<GameState2P>();
-        		for (Map.Entry<GameState2P, List<Integer>> entry : playedMoves.entrySet()){
-        			int tempN = entry.getValue().get(0);
-        			int tempW = entry.getValue().get(1);
-        			double tempUBC1 = calcUBC1(tempN, tempW, totalSims);
-        			if (ubc1 < tempUBC1){
-        				ubc1 = tempUBC1;
-        				n = tempN;
-        				w = tempW;
-        				l.clear();
-        				l.add(entry.getKey());
-        			} else if (ubc1 == tempUBC1){
-        				l.add(entry.getKey());
-        			}
-        		}
-    			stateToPlay = l.get(random.nextInt(l.size()));
-        	}
+        	GameState2P stateToPlay = getUCB1State(playedStates, legalMoves, totalSims);
         	int n = 1;
         	int w = (playGame(stateToPlay, stopTime, index)) ? 1 : 0;
-        	if (playedMoves.containsKey(stateToPlay)){
-    			n = playedMoves.get(stateToPlay).get(0) + 1;
-    			w = playedMoves.get(stateToPlay).get(1) + w;
+        	if (playedStates.containsKey(stateToPlay)){
+    			n = playedStates.get(stateToPlay).get(0) + 1;
+    			w = playedStates.get(stateToPlay).get(1) + w;
     		}
-        	playedMoves.put(stateToPlay, Arrays.asList(n, w));
+        	playedStates.put(stateToPlay, Arrays.asList(n, w));
         	
         }
 
-		GameState2P state = getBestState(playedMoves);
+		GameState2P state = getBestState(playedStates);
 		game.doMove(index, state);
 		
+	}
+	
+	private GameState2P getUCB1State(Map<GameState2P, List<Integer>> playedStates, List<Move> legalMoves, int totalSims){
+		boolean unplayedMove = false;
+    	GameState2P stateToPlay = null;
+    	List<GameState2P> legalStates = new ArrayList<GameState2P>();
+    	for (Move m : legalMoves){
+    		GameState2P s = m.doMove(state);
+    		legalStates.add(s);
+    		if (!playedStates.containsKey(s)){
+    			// play this move
+    			unplayedMove = true;
+    			stateToPlay = s;
+    		}
+    	}
+    	if (!unplayedMove){
+    		double ubc1 = -1;
+    		List<GameState2P> l = new ArrayList<GameState2P>();
+    		for (Map.Entry<GameState2P, List<Integer>> entry : playedStates.entrySet()){
+    			int n = entry.getValue().get(0);
+    			int w = entry.getValue().get(1);
+    			double tempUBC1 = calcUBC1(n, w, totalSims);
+    			if (ubc1 < tempUBC1){
+    				ubc1 = tempUBC1;
+    				l.clear();
+    				l.add(entry.getKey());
+    			} else if (ubc1 == tempUBC1){
+    				l.add(entry.getKey());
+    			}
+    		}
+			stateToPlay = l.get(random.nextInt(l.size()));
+    	}
+		return stateToPlay;
 	}
 	
 	private double calcUBC1(int n, int w, int totalSims){
